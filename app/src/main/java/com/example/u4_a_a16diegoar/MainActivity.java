4b82a78bee3a4eed3e07fe1364ad82b310b3b210
a16diegoar <a16diegoar@iessanclemente.net>
2019-10-21 19:51:49 +0200

    commit inicial

---
+++ app/src/main/java/com/example/u4_a_a16diegoar/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.u4_a_a16diegoar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void escribir(View v) {
+
+    }
+
+    public void showDatos(View v) {
+
+    }
+}
